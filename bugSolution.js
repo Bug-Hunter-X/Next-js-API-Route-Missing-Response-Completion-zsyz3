396980776051feaa128d4e190df18ba8860bbf87
BugHunter-X <askzenai@gmail.com>
2025-02-18 12:46:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+// pages/api/hello.js
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' });
+  res.end(); // Added to complete the response
+}
+```
